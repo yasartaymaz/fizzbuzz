43bdc688d199129570e755b2459d97d6abb9f93c
yasartaymaz.home <yasartaymaz@windowslive.com>
2022-01-02 21:18:28 +0300

    all files

---
+++ FizzBuzz/Helper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class Helper
+    {
+        public static string AskFizzBuzz(int number)
+        {
+            string result = null;
+
+            result = AskFizz(number);
+            if (result == number.ToString())
+            {
+                result = AskBuzz(number);
+            }
+            else
+            {
+                if (AskBuzz(number) == "buzz")
+                {
+                    result += AskBuzz(number);
+                }
+            }
+
+            return result;
+        }
+        public static string AskFizz(int number)
+        {
+            string result = number.ToString();
+
+            if (number % 3 == 0)
+            {
+                result = "fizz";
+            }
+
+            return result;
+        }
+
+        public static string AskBuzz(int number)
+        {
+            string result = number.ToString();
+
+            if (number % 5 == 0)
+            {
+                result = "buzz";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using FizzBuzz;
+
+Console.WriteLine("Hello, World!");
+
+for (int i = 1; i < 100; i++)
+{
+    string number = i.ToString();
+    
+    number = Helper.AskFizzBuzz(i);
+
+    Console.WriteLine(number);
+}
+
+
+Console.ReadLine();
